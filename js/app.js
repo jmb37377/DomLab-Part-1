fd59f295b8e20430a0e47af7f740051491d5fdef
jmb37377 <jmb37377@gmail.com>
2023-10-01 22:48:01 -0400

    part 2

--- js/app.js
+++ js/app.js
@@ -12,4 +12,67 @@ menulinks.array.forEach(function(link) {
     linkEl.setAttribute('href,link.href');
     topMenuEl.appenChild(linkEl);
 
-});//having trouble linking to html 
+});
+///Part 2
+SubmitEl=document.getElementById('sub-menu');
+subMenuEl.style.height=100%;
+subMenuEl.style.position='absolute';
+subMenuEl.style.top='0';
+const topMenuLinks=
+document.querySelectorAll('#top-menu a');
+let showingSubMenu=false;
+topMenuEl.addEventListener('click',function(evt) {
+    evt.preventDefault();
+    const link= evt.target;
+    if (link.tagName !== 'A') return;
+    console.log(link.textcontent);
+
+    if (link.classList.contains('active'))
+    link.classList.remove('active')
+});
+
+link.classList.remove('active');
+
+const linkData =
+menuLinks.find(function(linkObj) {
+    return linkObj.text ===
+    link.textContent;
+} ) ;
+
+showingSubMenu ='subLinks' in linkdata;
+
+if (showingSubMenu) {
+    buildingSubMenu(linkData.sublinks);
+    subMenuEl.style.top = '100%'
+} else {
+    subMenuEl.style.top = '0';
+    mainEl.innerHTML = '<h1>about</h1>';
+
+}
+
+function buildingSubMenu(subLinks) {
+    subMenuEl.innerHTML = '';
+    subLinks.forEach(function(link) {
+        const linlEL=
+        document.createElement('a');
+        linkEl.setAttribute('href', link.href);
+        linkEl.textContent=link.text;
+    });
+
+}
+
+subMenuEl.addEventListener('click', function(evt) {
+    evt.preventDefault();
+    const link=evt.target;
+    if (link.tagName !=='A') return;
+    console.log(link.textContent);
+
+}
+topMenuLinks.forEach(function(link) {
+    link.classList.remove('active');
+});
+
+mainEl.innerHTML=
+<h1>${link.textContent}</h1>
+)
+})
